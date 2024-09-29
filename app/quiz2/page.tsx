3ead53c3479b18720db2ffb4cd5377c2729bbc7e
AshleighWong <ashleightwong@gmail.com>
2024-09-28 22:20:10 -0400

    created quiz 2 route and

---
+++ app/quiz2/page.tsx
@@ -0,0 +1,10 @@
+import DynamicQuiz from "@/components/dynamicQuiz";
+
+export default function QuizPage() {
+  return (
+    <div>
+      <h1>Career Path Quiz</h1>
+      <DynamicQuiz />
+    </div>
+  );
+}
